5374e9efb9d9ed8b0ee0b31f096b4bf4e640c189
remote_vase <web.articlerequests@gmail.com>
2017-02-13 15:44:37 -0500

    Many changes
    **No need for commands yet.
    
    Fix the onDisable and onEnable messages
    
    Spaces and Update rank format
    
    Add custom regular player message
    
    That should be all of the Use statements that we need XD

--- src/BoxofDevs/BoxCore/main.php
+++ src/BoxofDevs/BoxCore/main.php
@@ -3,118 +3,81 @@
 namespace BoxofDevs\BoxCore;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
 use pocketmine\scheduler\PluginTask;
-use pocketmine\level\Level;
-use pocketmine\level\Position;
-use pocketmine\math\Vector3;
-use pocketmine\Server;
-use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\event\player\PlayerPreLoginEvent;
-use pocketmine\event\player\PlayerChatEvent;
-use pocketmine\event\player\PlayerMoveEvent;
-use pocketmine\tile\Tile;
-use pocketmine\item\Item;
-use pocketmine\utils\Config;
-use pocketmine\utils\TextFormat as C;
-use pocketmine\command\Command;
+use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\utils\Config;
+use pocketmine\math\Vector3;
+use pocketmine\level\Position;
+use pocketmine\Player;
+use pocketmine\block\Block;
+use pocketmine\tile\Sign;
+use pocketmine\level\Level;
+use pocketmine\item\Item;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\entity\Effect;
+use pocketmine\event\entity\EntityLevelChangeEvent ; 
+use pocketmine\tile\Chest;
+use pocketmine\inventory\ChestInventory;
+use pocketmine\event\plugin\PluginEvent;
 
 class main extends PluginBase implements Listener {
 	
 	public $prefix = "";
-	public $console = C::DARK_RED."You can only run this Command In-Game.";
 
-    public function onLoad(){
-		$this->getLogger()->info(C::YELLOW."Box is opening..");
-	}
-
-    public function onEnable(){
-		$this->getLogger()->info(C::GREEN."Box opened!");
+	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this ,$this);
 		$config = $this->getConfig();
 		$this->prefix = $config->get("Prefix")." ";
 		$this->saveDefaultConfig();
+	    	$this->getLogger()->info(C::GREEN."BoxCore has loaded!");
 	}
 	
-	/**
-	* @param PlayerPreLoginEvent $event
-	*/
-	public function PreLogin(PlayerPreLoginEvent $event){
-		$config = $this->getConfig();
-		$player = $event->getPlayer();
-		if($config->get("AlwaysSpawn") == "true"){
-			$player->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
-		}
-	}
-	
+
 	public function onJoin(PlayerJoinEvent $event){
 		$config = $this->getConfig();
-		#$event->setJoinMessage("");
+		#$event->setJoinMessage(""); //This could be useful in the future!
 		$player = $event->getPlayer();
 		$this->setRank($player);
 	}
 	
 	public function onQuit(PlayerQuitEvent $event){
 		$config = $this->getConfig();
-		#$event->setQuitMessage("");
+		#$event->setQuitMessage(""); //This could be useful in the future!
 		$player = $event->getPlayer();
 	}
-	
+
 	public function setRank(Player $player){
-		$name = $player->getName();
-		$rankyml = new Config($this->getDataFolder()."/rank.yml", Config::YAML);
-		$rank = $rankyml->get($name);  
-		if($rank == "Owner"){
-			$player->setNameTag(C::DARK_PURPLE."Owner".C::GRAY." | ".C::AQUA.$name);
+		$rankyml = new Config($this->getDataFolder() . "/rank.yml", Config::YAML);
+       		$rank = $rankyml->get($player->getName());
+	    	$player = $rankyml->get("RegularPlayerName")." ";
+       		if($rank == "Owner"){
+			$player->setNameTag(C::DARK_PURPLE."Owner".C::GRAY." | ".C::AQUA. $player->getName());
 		}
 		if($rank == "Co-Owner"){
-			$player->setNameTag(C::GREEN."Co-Owner:".C::GRAY." | ".C::AQUA.$name);
+			$player->setNameTag(C::GREEN."Co-Owner:".C::GRAY." | ".C::AQUA. $player->getName());
 		}
 		if($rank == "Admin"){
-			$player->setNameTag(C::DARK_BLUE."Admin".C::GRAY." | ".C::AQUA.$name);
+			$player->setNameTag(C::DARK_BLUE."Admin".C::GRAY." | ".C::AQUA. $player->getName());
 		}
 		if($rank == "Builder"){
-			$player->setNameTag(C::YELLOW."Builder".C::GRAY." | ".C::AQUA.$name);
+			$player->setNameTag(C::YELLOW."Builder".C::GRAY." | ".C::AQUA. $player->getName());
 		}
 		if($rank == "VIP"){
-			$player->setNameTag(C::GOLD."VIP".C::GRAY." | ".C::AQUA.$name);
+			$player->setNameTag(C::GOLD."VIP".C::GRAY." | ".C::AQUA. $player->getName());
 		}else{
-			$player->setNameTag(C::YELLOW."Player".C::GRAY." | ".C::AQUA.$name);
+			$player->setNameTag(C::YELLOW."$player".C::GRAY."| ".C::AQUA. $player->getName());
 		}
 	}
-	
-	/**
-	* @param CommandSender $sender
-	* @param Command $cmd
-	* @param string $label
-	* @param array $args
-	* @return bool
-	*/
-	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-		$name = $sender->getName();
-		switch($cmd->getName()){
-			case "bod":
-				if($sender instanceof Player){
-					$sender->sendMessage("This server uses BoxCore from BoxOfDevs.");
-				}else{
-					$sender->sendMessage(C::GOLD."Visit BoxOfDevs on Github: ".C::GRAY."https://github.com/BoxOfDevs");
-				}
-				return true;
-			case "lobby":
-				if($sender instanceof Player){
-					$sender->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
-					$sender->sendMessage(C::GREEN."Teleporting..");
-				}else{
-					$sender->sendMessage($this->console);
-				}
-				return true;
-		}
-	}
-
-    public function onDisable(){
-		$this->getLogger()->info(C::RED."Closing Box!");
+	public function onDisable(){
+		$this->getLogger()->info(C::RED."Shutting down BoxCore!");
 	}
 }
