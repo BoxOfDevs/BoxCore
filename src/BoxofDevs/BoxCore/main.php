0bb107b2263d92663582f721fa56cd98042ea2a2
TheRoyalBlock <remote.vase@gmail.com>
2017-02-22 09:52:12 +0800

    Add in remotevase's HomeTP stuff- Or most of it... lol

--- src/BoxofDevs/BoxCore/main.php
+++ src/BoxofDevs/BoxCore/main.php
@@ -86,6 +86,8 @@ class main extends PluginBase implements Listener {
 		$config = $this->getConfig();
 		$this->prefix = $config->get("Prefix")." ";
 		$this->saveDefaultConfig();
+		$this->homeData = new Config($this->getDataFolder()."/homes.yml", Config::YAML, array());
+		$this->saveResource("/homes.yml");
 		$this->getLogger()->info(C::GREEN."BoxCore has loaded!");
 	}
 	
@@ -125,8 +127,62 @@ class main extends PluginBase implements Listener {
 			$player->setNameTag(C::YELLOW."$player".C::GRAY."| ".C::AQUA. $player->getName());
 		}
 	}
-	
+	public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        switch(strtolower($command->getName())){
+            
+            case "home":
+            if($sender instanceof Player && $sender->isOp()){
+                $home = $this->homeData->get($args[0]);
+                if($home["world"] instanceof Level){
+                    $sender->setLevel($home["world"]);
+                    $sender->teleport(new Position($home["x"], $home["y"], $home["z"]));
+                    $sender->sendMessage(C::BLUE."You teleported home.");
+                }else{
+                    $sender->sendMessage(C::RED."That world is not loaded or Doesn't Exist!");
+                }
+                $sender->sendMessage(C::RED."You must be an op to issue this command");
+            }else{
+                if(!$sender->isOp()){
+                    $sender->sendMessage(C::RED."Please run command in-game.");
+                }
+            }
+            break;
+            
+            case "sethome":
+            if ($sender instanceof Player){
+                if($sender->isOp()){
+                    $x = $sender->x;
+                    $y = $sender->y;
+                    $z = $sender->z;
+                    $level = $sender->getLevel();
+                    // $args[0] is the Name of the house -> /sethome <name>
+                    $this->homeData->set($args[0], array(
+                        "x" => $x,
+                        "y" => $y,
+                        "z" => $z,
+                        "world" => $level,
+                    ));
+                    $sender->sendMessage(C::GREEN."Your home is set at coordinates\n" . "X:" . C::YELLOW . $x . C::GREEN . "\nY:" . C::YELLOW . $y . C::GREEN . "\nZ:" . C::YELLOW . $z . C::GREEN . "\nUse /home < ". $args[0] ." > to teleport to this home!");
+                    $this->getLogger()->info($sender->getName() . " has set their home in world " . $sender->getLevel()->getName());
+                }
+                $sender->sendMessage(C::RED."You must be an op to issue this command");
+            }
+            $sender->sendMessage(C::RED. "Please run command in game.");
+            break;
+            
+            case "ishome":
+                $home = $this->homeData->get($args[0]);
+                if($home["world"] instanceof Level){
+                    $sender->sendMessage(C::BLUE."Yes, " . $args[0] . "is a house. It's location is " . $home["x"] ." " . $home["y"] . " " . $home["z"]. "In the world " . $home["world"]);
+                }else{
+                    $sender->sendMessage(C::BLUE. "No,-" . $args[0] . "-is not a house. Use the /sethome command to set a home with this name.");
+                }
+            break;
+        }
+        return true;
+    }
 	public function onDisable(){
+		$this->saveResource("/homes.yml");
 		$this->getLogger()->info(C::RED."Shutting down BoxCore!");
 	}
 }
