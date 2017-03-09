c171fd14579cb4d1f688e012b84c2aa003c21245
TheRoyalBlock <turbinenetwork@gmail.com>
2017-03-09 18:42:36 -0500

    Starting dat

---
+++ src/BoxofDevs/BoxCore/Chat/ChatTask.php
@@ -0,0 +1,16 @@
+<?php
+namespace BoxOfDevs\BoxCore\Chat;
+use pocketmine\utils\TextFormat;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\utils\Utils;
+use pocketmine\Player;
+class ChatTask extends PluginTask {
+    public function __construct($owner) {
+        $this->owner = $owner;
+    }
+    public function onRun($currentTick) {
+        $this->filter = new \ChatTask\Chat();
+        $this->filter->clearRecentChat();
+    }
+}
